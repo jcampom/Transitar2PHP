1e2fda9fa3388cdc9d03a474265c00ea163aebf1
cms-juan <cms-juan@cienagamovilidadsegura.com>
2024-06-06 17:26:06 -0500

    Se arregla crear_usuarios.php

--- crear_usuarios.php
+++ crear_usuarios.php
@@ -3,8 +3,6 @@ include 'menu.php';
 
 // echo '\nJLCM:crear_usuarios.php : _GET["activo"]  = '. $_GET["activo"];
 // echo '\nJLCM:crear_usuarios.php : _GET["eliminar"] = '. $_GET["eliminar"];
-print_r($_POST);
-
 if(!empty($_GET["activo"])){
     $editar="UPDATE usuarios SET estado = '".$_GET['cambio']."' where id = '".$_GET['activo']."' and empresa = '$empresa'";
       $resultadoedit=sqlsrv_query( $mysqli,$editar, array(), array('Scrollable' => 'buffered'));
