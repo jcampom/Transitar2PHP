f5817e4d2cc8116f12f7ec02efd9a8f31afeee07
jcampom <jlcm1972@gmail.com>
2024-07-08 16:11:27 -0500

    Create 20240708_DML_tablas.sql
    Inserción de datos en la tabla   TABLAS

---
+++ _scripts/20240708_DML_tablas.sql
@@ -0,0 +1,23 @@
+PRINT 'INSERT:tablas'
+truncate table u859387114_transitar..tablas;
+SET IDENTITY_INSERT u859387114_transitar..tablas ON
+
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (5, 'gruas', 1, 0, '2023-06-13', '2023-06-13 15:56:19');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (6, 'marca', 1, 0, '2023-06-13', '2023-06-13 16:15:39');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (7, 'lineas', 1, 0, '2023-06-13', '2023-06-13 16:16:38');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (8, 'color', 1, 0, '2023-06-13', '2023-06-13 16:16:58');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (9, 'toneladas', 1, 0, '2023-06-13', '2023-06-13 16:19:07');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (10, 'tercero', 1, 0, '2023-06-13', '2023-06-13 16:28:07');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (11, 'tipos', 1, 0, '2023-06-13', '2023-06-13 16:28:42');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (12, 'empresa_transporte', 1, 0, '2023-06-13', '2023-06-13 16:51:21');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (13, 'bancos', 1, 0, '2023-06-13', '2023-06-13 17:15:06');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (14, 'cuentas', 1, 0, '2023-06-13', '2023-06-13 17:31:22');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (15, 'conceptos', 1, 0, '2023-06-13', '2023-06-13 18:04:02');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (17, 'areas', 1, 0, '2023-06-13', '2023-06-13 21:19:33');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (18, 'sedes', 1, 0, '2023-06-13', '2023-06-13 21:21:52');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (19, 'empleados', 1, 0, '2023-06-13', '2023-06-13 21:23:03');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (20, 'tipo_inventario', 1, 0, '2023-06-13', '2023-06-13 21:59:06');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (21, 'items_inventario', 1, 0, '2023-06-13', '2023-06-13 21:59:38');
+INSERT INTO u859387114_transitar..tablas (id, nombre, usuario, empresa, fecha, fechayhora) VALUES (22, 'estado_inventario', 1, 0, '2023-06-13', '2023-06-13 21:59:49');
+
+SET IDENTITY_INSERT u859387114_transitar..tablas OFF
