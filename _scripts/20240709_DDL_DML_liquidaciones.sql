b1ccbf9a52285ccbb15f24ce0058529cd5e6444b
jcampom <jlcm1972@gmail.com>
2024-07-09 15:22:26 -0500

    Update 20240709_DDL_DML_liquidaciones.sql
    Se corrige la tabla en la sentencia SET IDENTITY INSERT ... OFF

--- _scripts/20240709_DDL_DML_liquidaciones.sql
+++ _scripts/20240709_DDL_DML_liquidaciones.sql
@@ -46,5 +46,5 @@ SET IDENTITY_INSERT u859387114_transitar..liquidaciones ON
 INSERT INTO u859387114_transitar..liquidaciones (id ,tipo_tramite ,ciudadano ,placa ,nota_credito ,tipo_servicio ,clase_vehiculo ,clasificacion_vehiculo ,comparendo ,estado ,usuario ,fecha ,fechayhora ,empresa)
 SELECT Tliquidacionmain_ID as id, Tliquidacionmain_tipodoc as tipo_tramite, Tliquidacionmain_idciudadano as ciudadano, Tliquidacionmain_placa as placa, Tliquidacionmain_nc as nota_credito, 0 as tipo_servicio, 0 as clase_vehiculo, 0 as clasificacion_vehiculo, 0 as comparendo, Tliquidacionmain_estado as estado, Tliquidacionmain_user as usuario, Tliquidacionmain_fecha as fecha, Tliquidacionmain_fecha as fechayhora, 1 as empresa
 FROM cienaga..Tliquidacionmain;
-SET IDENTITY_INSERT u859387114_transitar..acuerdosp_periodos OFF
+SET IDENTITY_INSERT u859387114_transitar..liquidaciones OFF
 ------------------------------------------------------------
