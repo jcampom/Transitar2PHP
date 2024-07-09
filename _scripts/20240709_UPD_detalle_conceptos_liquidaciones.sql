7148751abef14d9eb1d02fd7e613cb200c42ea3e
jcampom <jlcm1972@gmail.com>
2024-07-09 18:16:31 -0500

    Create 20240709_UPD_detalle_conceptos_liquidaciones.sql
    Actualización de las columnas comparendo, acuerdo de pago (cuota = id del acuerdo de pago)  y dt (derechos de tránsito)

---
+++ _scripts/20240709_UPD_detalle_conceptos_liquidaciones.sql
@@ -0,0 +1,30 @@
+USE u859387114_transitar
+GO
+-------------------------------------------------------------
+update d
+set d.comparendo = (
+select  tc.Tliqconcept_doc 
+from cienaga..Tliqconcept tc
+where tc.Tliqconcept_tipodoc=4 --comparendos
+and tc.Tliqconcept_ID = d.id
+)
+from u859387114_transitar..detalle_conceptos_liquidaciones d
+-------------------------------------------------------------
+update d
+set d.cuota = (
+select  tc.Tliqconcept_doc 
+from cienaga..Tliqconcept tc
+where tc.Tliqconcept_tipodoc=6   --acuerdos
+and tc.Tliqconcept_ID = d.id
+)
+from u859387114_transitar..detalle_conceptos_liquidaciones d
+-------------------------------------------------------------
+update d
+set d.dt = (
+select  tc.Tliqconcept_doc 
+from cienaga..Tliqconcept tc
+where tc.Tliqconcept_tipodoc =7 --derechos de tránsito
+and tc.Tliqconcept_ID = d.id
+)
+from u859387114_transitar..detalle_conceptos_liquidaciones d
+------------------------------------------------------------
